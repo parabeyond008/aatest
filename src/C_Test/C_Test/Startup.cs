b512be4a73e7d92534dae2fb7067477389d84ece
parabeyond008 <parabeyond008@github.com>
2020-09-28 10:28:30 +0800

    111

--- src/C_Test/C_Test/Startup.cs
+++ src/C_Test/C_Test/Startup.cs
@@ -27,6 +27,8 @@ namespace C_Test
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
